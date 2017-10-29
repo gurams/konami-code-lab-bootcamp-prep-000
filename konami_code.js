2dbab671ab5ff74509a8e57f6c67475c8223dc6c
Ramesh <gurams@gmail.com>
2017-10-29 03:19:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,6 @@ input.addEventListener('keydown', function(e) {
 })
 }
 // Key codes for A, B, and C keys.
-const alphabet = [65, 66, 67];
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 // Keep track of index outside of the event handler.
